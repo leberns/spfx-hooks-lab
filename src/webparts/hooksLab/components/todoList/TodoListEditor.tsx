93aafe03dcd4ad8298ec8682e1c79b9cdec8a6a9
Leandro Bernsmueller <lebernsmuller@hotmail.com>
2021-01-30 13:24:03 +0100

    refactored local storage into the custom hook storage

--- src/webparts/hooksLab/components/todoList/TodoListEditor.tsx
+++ src/webparts/hooksLab/components/todoList/TodoListEditor.tsx
@@ -2,31 +2,60 @@ import * as React from 'react';
 import { ChangeEvent, useState, useEffect, useRef } from 'react';
 import { ITodoItem } from './ITodoItem';
 
-export default function TodoList() {
-  const initialTodos = () => {
-    const persistedTodos: ITodoItem[] = JSON.parse(window.localStorage.getItem('todos')) || [];
-    return persistedTodos;
+type StorageReturnType =
+  [ITodoItem[], (v: ITodoItem[]) => void, () => void, (item: ITodoItem) => void];
+
+function useStorage(key: string, persistedCallback: (v: ITodoItem[]) => void): StorageReturnType {
+
+  const initializeStorage = () => {
+    const persistedValue: ITodoItem[] = JSON.parse(window.localStorage.getItem(key)) || [];
+    return persistedValue;
   };
 
-  const [text, updateText] = useState('');
-  const [priority, updatePriority] = useState(1);
-  const [todos, updateTodos] = useState(initialTodos);
-  const [countTodos, updateCountTodos] = useState(0);
-  const [countPendingITems, updateCountPendingITems] = useState(0);
-  const textRef = useRef<HTMLInputElement>();
+  const [storageValue, setStorageState] = useState(initializeStorage);
 
-  const updateStats = () => {
-    updateCountTodos(todos.length);
-    updateCountPendingITems(todos.filter(i => !i.isDone).length);
-    console.log(`Count of pending items: ${countPendingITems}, total: ${countTodos}`);
-  };
+  function setStorage(v: ITodoItem[]) {
+    setStorageState(v);
+  }
+
+  function clearStorage() {
+    setStorageState([]);
+  }
 
   useEffect(() => {
-    const todosPersist = JSON.stringify(todos);
-    window.localStorage.setItem('todos', todosPersist);
-    console.log(todosPersist);
-    updateStats();
-  }, [todos]);
+    const persistValue = JSON.stringify(storageValue);
+    window.localStorage.setItem(key, persistValue);
+    console.log(persistValue);
+    if (persistedCallback) {
+      persistedCallback(storageValue);
+    }
+  }, [storageValue]);
+
+  const setDone = (item: ITodoItem) => {
+    setStorageState(prevItems =>
+      prevItems.map(t =>
+        item.id === t.id ? { ...item, isDone: !item.isDone } : t
+      ));
+  };
+
+  return [storageValue, setStorage, clearStorage, setDone];
+}
+
+export default function TodoList() {
+
+  const [countItems, setCountItems] = useState(0);
+  const [countPendingITems, setCountPendingItems] = useState(0);
+  const updateStats = (items: ITodoItem[]) => {
+    setCountItems(items.length);
+    const countPending = items.filter(i => !i.isDone).length;
+    setCountPendingItems(countPending);
+    console.log(`Count of pending items: ${countPendingITems}, total: ${countItems}`);
+  };
+
+  const [todos, setTodos, clearTodos, setDone] = useStorage('todos', updateStats);
+  const textRef = useRef<HTMLInputElement>();
+  const [text, setText] = useState('');
+  const [priority, setPriority] = useState(1);
 
   const addItem = () => {
     if (text.trim().length === 0) {
@@ -45,32 +74,27 @@ export default function TodoList() {
     newTodos.push(item);
     const newTodosOrdered = newTodos.sort((a: ITodoItem, b: ITodoItem) =>
       (a.priority >= b.priority ? 1 : -1));
-    updateTodos(newTodosOrdered);
+    setTodos(newTodosOrdered);
 
-    updateText('');
-    updatePriority(1);
-  };
-
-  const setDone = (todo: ITodoItem) => {
-    updateTodos(prevTodos =>
-      prevTodos.map(t =>
-        todo.id === t.id ? { ...todo, isDone: !todo.isDone } : t
-      ));
+    setText('');
+    setPriority(1);
   };
 
   const removeDone = () => {
     const pendingTodos = todos.filter((todoItem) => !todoItem.isDone);
-    updateTodos(pendingTodos);
+    setTodos(pendingTodos);
   };
 
-  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => updateText(e.target.value);
-  const onChangePriority = (e: ChangeEvent<HTMLInputElement>) => updatePriority(+e.target.value);
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+  const onChangePriority = (e: ChangeEvent<HTMLInputElement>) => setPriority(+e.target.value);
   const onClickAdd = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     addItem();
   };
   const onClickDone = (todo: ITodoItem) => setDone(todo);
   const onClickRemoveDone = () => removeDone();
+  const onClickRemoveAll = () => clearTodos();
 
   return (
     <section>
@@ -93,6 +117,7 @@ export default function TodoList() {
       </div>
       <button onClick={onClickAdd.bind(this)}>Add</button>
       <button onClick={onClickRemoveDone.bind(this)}>Remove Done</button>
+      <button onClick={onClickRemoveAll.bind(this)}>Remove All</button>
       <div>
         {todos.map(todo => (
           <div key={todo.id} onClick={onClickDone.bind(this, todo)}>
@@ -104,7 +129,7 @@ export default function TodoList() {
           </div>
         ))}
       </div>
-      <div>There are still {countPendingITems} item(s) pending from a total of {countTodos}</div>
+      <div>There are still {countPendingITems} item(s) pending from a total of {countItems}</div>
     </section>
   );
 }
